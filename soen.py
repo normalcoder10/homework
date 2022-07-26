a0767b7f017c1b55ed2b5088d4ce4eec8eaf1ee7
normalcoder10 <ynks029@gmail.com>
2022-07-26 23:47:17 +0900

    first commit

---
+++ soen.py
@@ -0,0 +1,30 @@
+number=int(input())
+output=''
+even=''
+a=len(str(number))
+b=0
+while a:
+    c=str(number)
+    if int(c[b])%2==0:
+        even=even+c[b]
+    else:
+        output=output+c[b]
+    b=b+1
+    a=a-1
+output=output+even
+print(output)
+
+number1=int(input())
+output1=''
+times=len(str(number1))
+index1=0
+while times:
+    f=str(number1)
+    e=str(f[index1])
+    if int(e)%2==0:
+        output1=output1+'X'
+    else:
+        output1=output1+e
+    index1=index1+1
+    times=times-1
+print(output1)
